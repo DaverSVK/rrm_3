747ed487cc1137700c3d490cbb51e1656be4c0cd
David <david.szepvolgyi@gmail.com>
2022-11-06 18:31:48 +0100

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/david/szepvolgyi_2_ws/src
+# Build directory: /home/david/szepvolgyi_2_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rrm_cv3_szepvolgyi")
